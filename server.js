18f44cbcec002087d0a29391f8d7971ea1a8f96a
Michael Diamond <94988620+mikeydgithub@users.noreply.github.com>
2022-03-05 14:13:03 -0500

    routing options added

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+const apiRoutes = require("./routes/apiroutes");
+const htmlRoutes = require("./routes/htmlroutes");
+
+app.use(express.urlencoded({ extend: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
+
+app.listen(PORT, () => {
+  console.log(`API server now on port ${PORT}!`);
+});
